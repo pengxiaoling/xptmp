e69f143a3daac419a6c0f5457c2d6d9318958028
xiaoling <xp@cabag.ch>
2014-09-13 22:26:11 +0200

    remove ext_tables.php

--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'xp Fluid Template');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:xptmp/Configuration/TypoScript/pageTsConfig.txt">');
